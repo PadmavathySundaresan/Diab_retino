48b85c28191ce2ae0baf6b95975162457c580686
PadmavathySundaresan <62243192+PadmavathySundaresan@users.noreply.github.com>
2020-05-26 15:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,33 +29,33 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-def prediction_from_Inception(image):
-    image_size1 = (299, 299)    
-    img = image.resize(image_size1)
-    x = keras.preprocessing.image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)    
-    x = preprocess_input(x)
-    feature = model_test1.predict(x)
-    flat = feature.flatten()
-    flat = np.expand_dims(flat, axis=0)
-    preds = classifier1.predict(flat)
-    print(preds)
-    return preds
-
-   
-
-#def prediction_from_MobileNet(image):
-#    image_size2 = (224, 224)      
-#    img = image.resize(image_size2)
+#def prediction_from_Inception(image):
+#    image_size1 = (299, 299)    
+#    img = image.resize(image_size1)
 #    x = keras.preprocessing.image.img_to_array(img)
 #    x = np.expand_dims(x, axis=0)    
 #    x = preprocess_input(x)
-#    feature = model_test2.predict(x)
+#    feature = model_test1.predict(x)
 #    flat = feature.flatten()
 #    flat = np.expand_dims(flat, axis=0)
-#    preds = classifier2.predict(flat)
+#    preds = classifier1.predict(flat)
 #    print(preds)
 #    return preds
+
+   
+
+def prediction_from_MobileNet(image):
+    image_size2 = (224, 224)      
+    img = image.resize(image_size2)
+    x = keras.preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)    
+    x = preprocess_input(x)
+    feature = model_test2.predict(x)
+    flat = feature.flatten()
+    flat = np.expand_dims(flat, axis=0)
+    preds = classifier2.predict(flat)
+    print(preds)
+    return preds
    
    
    
@@ -83,17 +83,17 @@ print("HI. WELCOME TO THE SERVER")
 # graph = tf.get_default_graph()
 # global model
 # print("MODEL LOADED!!!")
-print('LOADING INCEPTION V3...')
-classifier1 = pickle.load(open(r'classifier_lr_idird_final.cpickle', 'rb')) #inception V3
-base_model1 = InceptionV3(include_top=False, weights='imagenet', input_tensor=Input(shape=(299,299,3)))
-model_test1 = Model(input=base_model1.input, output=base_model1.layers[-1].output)
-print('INCEPTION V3 MODEL LOADED!!')
+#print('LOADING INCEPTION V3...')
+#classifier1 = pickle.load(open(r'classifier_lr_idird_final.cpickle', 'rb')) #inception V3
+#base_model1 = InceptionV3(include_top=False, weights='imagenet', input_tensor=Input(shape=(299,299,3)))
+#model_test1 = Model(input=base_model1.input, output=base_model1.layers[-1].output)
+#print('INCEPTION V3 MODEL LOADED!!')
 
-#print('LOADING MOBILENET...')
-#classifier2 = pickle.load(open(r'classifier_mobilenet.cpickle', 'rb'))
-#base_model2 = MobileNet(include_top=False, weights='imagenet', input_tensor=Input(shape=(224,224,3)), input_shape=(224,224,3))
-#model_test2 = Model(input=base_model2.input, output=base_model2.layers[-1].output)
-#print('MOBILENET MODEL LOADED!!')
+print('LOADING MOBILENET...')
+classifier2 = pickle.load(open(r'classifier_mobilenet.cpickle', 'rb'))
+base_model2 = MobileNet(include_top=False, weights='imagenet', input_tensor=Input(shape=(224,224,3)), input_shape=(224,224,3))
+model_test2 = Model(input=base_model2.input, output=base_model2.layers[-1].output)
+print('MOBILENET MODEL LOADED!!')
 
 #print('LOADING XCEPTION...')
 #classifier3 = pickle.load(open(r'classifier1 (1).cpickle', 'rb'))
@@ -113,11 +113,11 @@ def doPrediction():
     image = Image.open(dataBytesIO)
    
     processed_image = preprocess_image(image)  
-    pred1 = prediction_from_Inception(processed_image)
-    #pred2 = prediction_from_MobileNet(processed_image)
+    #pred1 = prediction_from_Inception(processed_image)
+    pred2 = prediction_from_MobileNet(processed_image)
     #pred3 = prediction_from_Xception(processed_image)
    
-    labels=[pred1]
+    labels=[pred2]
     print(labels)    
    
     labels = np.array(labels)
